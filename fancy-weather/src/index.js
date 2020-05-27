34cff28a48cd22ba57758d0a34c5d1c37a61eee9
Viktoryia Kiyanka <mikisum@mail.ru>
2020-05-27 16:43:47 +0300

    feat: add weather api

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,8 +1,9 @@
-
+import "./style.css";
 const elementToTranslate = document.querySelectorAll('[data-i18n]');
-const search = document.getElementById('search');
+const searchInput = document.getElementById('searchInput');
 const changeLanguageButton = document.getElementById('languageButton');
 const languageButton = document.querySelectorAll('.dropdown-item');
+const buttonSearch = document.getElementById('buttonSearch');
 
 const languages = {
     EN : "en.json",
@@ -10,13 +11,13 @@ const languages = {
     BE : "be.json",
 }
 function translate(lang) {
-    return fetch(languages[lang])
+    return fetch(`languages/${languages[lang]}`)
             .then(res => res.json())
             .then(langFile => {
                 elementToTranslate.forEach(element => {
                 element.textContent = langFile[element.dataset.i18n];
                 });
-                search.setAttribute("placeholder", langFile['search']);
+                searchInput.setAttribute("placeholder", langFile['search']);
             });
 }
 
@@ -27,5 +28,77 @@ document.addEventListener('click', (event) => {
             changeLanguageButton.innerText = event.target.innerText;
             translate(changeLanguageButton.innerText);
     }
+    if (event.target.id === 'buttonSearch') {
+        event.preventDefault(); 
+        getCurrentWeather();
+        getLocation();
+    }
+});
+const apiKey = 'd9cbddc7739840c4bd5122238202605';
+const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Minsk`
+const weatherDaysUrl =  `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=07112&days=7`
+
+const weatherText = document.getElementById('text');
+const feelslike = document.getElementById('feelslike');
+const wind = document.getElementById('wind');
+const humidity = document.getElementById('humidity');
+
+function getCurrentWeather() {
+    return fetch(weatherUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            weatherText.innerText = data.current.condition.text;
+            feelslike.innerText = data.current.feelslike_c;
+            wind.innerText = data.current.wind_mph;
+            humidity.innerText = data.current.humidity;
+            setWeatherInfo(data);
+        });    
+};
+
+
+const city = document.getElementById('city');
+const country = document.getElementById('country');
+
+function setWeatherInfo(data) {
+    city.innerText = data.location.name;
+    country.innerText = data.location.country;
+}
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlraXN1bSIsImEiOiJja2FvYmE0cHQwcDN0MnlwZmppNnQ4c21vIn0.hw0Slxj8zfqIGrT6TFkagw';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [27.56, 53.9],
+  zoom: 9
 });
 
+
+function getLocation() {
+const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchInput.value}.json?&autocomplete=false&access_token=${mapboxgl.accessToken}`;
+    return fetch(locationUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            // setWeatherInfo(data);
+        });    
+};
+
+
+
+
+// function searchCity() {
+//     let searchCity = searchInput.value;
+//     if (!searcCity) {
+//         searchCity = 'Minsk';
+//     }
+// }
+// function submit(event) {
+//     if (event) {
+//         event.preventDefault();
+//     }
+//     getCurrentWeather();
+// }
+// searchButton.addEventListener('submit', submit);
