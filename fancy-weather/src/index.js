afc2888bf473208d0edfbdd9bd2207b6f7d6f096
Viktoryia Kiyanka <mikisum@mail.ru>
2020-05-27 22:16:34 +0300

    feat: add searching geolocation

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -10,6 +10,28 @@ const languages = {
     RU : "ru.json",
     BE : "be.json",
 }
+
+window.addEventListener('load', () =>{
+    getPosition();
+});
+const latitudeHtml = document.getElementById('latitude');
+const longitudeHtml = document.getElementById('longitude');
+function getPosition() {
+let latitude;
+let longitude;
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            latitude  = position.coords.latitude;
+            longitude = position.coords.longitude;
+            console.log(position.coords);
+            map.flyTo({ center: [longitude, latitude] });
+            latitudeHtml.innerText = 'Latitude:' + latitude;
+        });
+    } 
+    getCurrentWeather(longitude, latitude);  
+   
+}
+
 function translate(lang) {
     return fetch(`languages/${languages[lang]}`)
             .then(res => res.json())
@@ -38,6 +60,7 @@ const apiKey = 'd9cbddc7739840c4bd5122238202605';
 const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Minsk`
 const weatherDaysUrl =  `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=07112&days=7`
 
+const temperature = document.getElementById('temperature');
 const weatherText = document.getElementById('text');
 const feelslike = document.getElementById('feelslike');
 const wind = document.getElementById('wind');
@@ -48,10 +71,13 @@ function getCurrentWeather() {
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
+            temperature.innerText = data.current.temp_c;
             weatherText.innerText = data.current.condition.text;
             feelslike.innerText = data.current.feelslike_c;
-            wind.innerText = data.current.wind_mph;
+            wind.innerText = `${Math.round(data.current.wind_kph * 1000 / 60)}`;
             humidity.innerText = data.current.humidity;
+            // latitudeHtml.innerText = `Latitude: ${data.location.lat}`;
+            // longitudeHtml.innerText = `Longitude: ${data.location.lon}`;
             setWeatherInfo(data);
         });    
 };
@@ -71,8 +97,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWlraXN1bSIsImEiOiJja2FvYmE0cHQwcDN0MnlwZmppN
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [27.56, 53.9],
-  zoom: 9
+//   center: [27.56, 53.9],
+  zoom: 11
 });
 
 
