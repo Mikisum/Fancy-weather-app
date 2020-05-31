1c4779b2994ee38940f4952c679f485873a999a0
Viktoryia Kiyanka <mikisum@mail.ru>
2020-05-31 23:04:07 +0300

    refactor: apply eslint

--- fancy-weather/src/days.js
+++ fancy-weather/src/days.js
@@ -1,15 +1,15 @@
 const days = {
-    0: 'Sunday',
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday',
-}
+  0: 'Sunday',
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+};
 
 function getDayName(dayNum) {
-    return dayNum < 7 ? days[dayNum] : days[dayNum - 7]
+  return dayNum < 7 ? days[dayNum] : days[dayNum - 7];
 }
 
 export default getDayName;
