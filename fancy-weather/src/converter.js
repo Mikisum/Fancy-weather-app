aba2b021650be981ec4e62f90178f2a90ca4d3cc
Viktoryia Kiyanka <mikisum@mail.ru>
2020-06-10 13:55:45 +0300

    refactor: get rid of magic strings

--- fancy-weather/src/converter.js
+++ fancy-weather/src/converter.js
@@ -1,18 +1,30 @@
 import domElements from './domElements';
 
-let temperatureUnits = 'C';
+const temperatureUnits = {
+  C: 'C',
+  F: 'F',
+};
+
+const hemisphere = {
+  W: 'W',
+  E: 'E',
+  S: 'S',
+  N: 'N',
+};
+
 function temperatureConverter(units) {
-  if (units !== temperatureUnits) {
+  if (units !== temperatureUnits.C) {
     domElements.temperatureElements.forEach((element) => {
-      if (units === 'F') {
-        const degree = parseFloat(element.innerText);
-        element.innerText = `${Math.round((degree * 1.8) + 32)}°`;
+      const unit = element;
+      if (units === temperatureUnits.F) {
+        const degree = parseFloat(unit.innerText);
+        unit.innerText = `${Math.round((degree * 1.8) + 32)}°`;
       } else {
-        const degree = parseFloat(element.innerText);
-        element.innerText = `${Math.round((degree - 32) / 1.8)}°`;
+        const degree = parseFloat(unit.innerText);
+        unit.innerText = `${Math.round((degree - 32) / 1.8)}°`;
       }
     });
-    temperatureUnits = units;
+    temperatureUnits.C = units;
   }
 }
 
@@ -21,25 +33,25 @@ function truncate(n) {
 }
 
 function getDMS(dd, longOrLat) {
-  let hemisphere;
+  let valueHemisphere;
   if (/^[WE]|(?:lon)/i.test(longOrLat)) {
     if (dd < 0) {
-      hemisphere = 'W';
+      valueHemisphere = hemisphere.W;
     } else {
-      hemisphere = 'E';
+      valueHemisphere = hemisphere.E;
     }
   } else if (dd < 0) {
-    hemisphere = 'S';
+    valueHemisphere = hemisphere.S;
   } else {
-    hemisphere = 'N';
+    valueHemisphere = hemisphere.N;
   }
   const absDD = Math.abs(dd);
   const degrees = truncate(absDD);
   const minutes = truncate((absDD - degrees) * 60);
   const seconds = ((absDD - degrees - minutes / 60) * 60 ** 2).toFixed(2);
 
-  const dmsArray = [degrees, minutes, seconds, hemisphere];
+  const dmsArray = [degrees, minutes, seconds, valueHemisphere];
   return `${dmsArray[0]}°${dmsArray[1]}'${dmsArray[2]}" ${dmsArray[3]}`;
 }
 
-export {getDMS, temperatureConverter} ;
+export { getDMS, temperatureConverter };
